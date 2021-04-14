6ee2268a063a5dfc5b8e7d5bf0726f4e21ff8ddd
luiscastro8p <luisarmandocastro12@gmail.com>
2021-04-13 20:01:37 -0600

    dist

---
+++ src/app/routes/routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from '@angular/router';
+import { LayoutComponent } from '../layout/layout.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('./home/home.module').then((m) => m.FeatureModule),
+      },
+    ],
+  },
+
+  {
+    path: '**',
+    redirectTo: '/home',
+  },
+];
+
+/* loadChildren: () =>
+      import('./home/home.module').then((m) => m.FeatureModule), */
